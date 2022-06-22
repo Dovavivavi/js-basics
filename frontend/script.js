2344d1dda1716d301c70d2e6e132e39d6e118ac1
Dovavivavi <szilagyiabel1@gmail.com>
2022-06-22 18:00:35 +0300

    edited js

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,14 @@ console.log('hello world');
 function loadEvent() {
     console.log('loaded');
 
+    let rootElement=(document.getElementById('root'))
+
     document.getElementById('root').innerHTML="sziasztok"
+    /* console.dir(document.getElementById('root'))
+    console.dir(document.querySelector('#root')) */
+
+    document.getElementById('root').innerHTML="cso"
+
 }
 
 window.addEventListener('click',loadEvent)
